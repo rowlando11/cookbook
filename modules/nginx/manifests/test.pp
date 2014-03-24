b38e8601dddc04478475086b0d5e1c039dd2bb35
rowlando <rowland.ono@gmail.com>
2014-03-24 11:47:42 +0000

    changes to config

---
+++ modules/nginx/manifests/test.pp
@@ -0,0 +1,19 @@
+class debianlike {
+  notify { 'Special manifest for Debian-like systems': }
+}
+class redhatlike {
+  notify { 'Special manifest for RedHat-like systems': }
+}
+case $::operatingsystem {
+  'Ubuntu',
+  'Debian': {
+    include debianlike
+  }
+  'RedHat',
+  'Fedora',
+  'CentOS': {
+    include redhatlike
+  }
+         default: {
+           notify { "I don't know what kind of system you have!": }
+} }
